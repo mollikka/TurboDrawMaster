d95bbf88c26018c7b09ba953f137ab46d07933a1
Lauri Tervonen <lauri.tervonen@aalto.fi>
2014-06-24 20:20:35 +0300

    color picker has a saturation slider

--- overlays/color_picker.py
+++ overlays/color_picker.py
@@ -9,10 +9,11 @@ class ColorPickerOverlay(Overlay):
         super(ColorPickerOverlay,self).__init__(user)
         self.name = "Color Picker"
 
-        self.buttons.append([ColorButton(self),(100,100)])
+        self.buttons.append([HueLumSurfaceButton(self),(100,100)])
+        self.buttons.append([SatSliderButton(self),(750,100)])
         self.buttons.append([ActiveColorButton(self),(0,100)])
 
-class ColorButton(Button):
+class HueLumSurfaceButton(Button):
 
     def __init__(self, color_picker_overlay):
         self.rect = pygame.Rect((0,0),(640,480))
@@ -36,6 +37,32 @@ class ColorButton(Button):
         for x in range(width):
             for y in range(height):
                 pygame.draw.line(colormap, self.coords_to_color(x,y), (x,y),(x,y))
+        return colormap
+
+class SatSliderButton(Button):
+
+    def __init__(self, color_picker_overlay):
+        self.rect = pygame.Rect((0,0),(20,480))
+        self.parent = color_picker_overlay
+
+    def left_click(self, mouse_x, mouse_y):
+        if self.rect.collidepoint((mouse_x,mouse_y)):
+            self.parent.user.active_color = self.coords_to_color(mouse_y)
+
+    def coords_to_color(self, y):
+        r,g,b = [i/255 for i in self.parent.user.active_color]
+        h,l,s = colorsys.rgb_to_hls(r,g,b)
+        width,height = self.rect.size
+        color = colorsys.hls_to_rgb(h,l,float(y)/height)
+        color = [i*255 for i in color]
+        return color
+
+    def draw(self):
+        width, height = self.rect.size
+        colormap = pygame.Surface(self.rect.size)
+
+        for y in range(height):
+            pygame.draw.line(colormap, self.coords_to_color(y), (0,y),(width,y))
         return colormap
 
 class ActiveColorButton(Button):
